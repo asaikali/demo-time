48d6c5980f9ca33fafb08a99ca68c6f95fba423c
Adib Saikali <asaikali@gmail.com>
2015-12-03 02:39:45 -0500

    demo time example application

---
+++ demo-time/src/main/java/com/example/DemoTimeApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoTimeApplication {
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Version 1: " + new Date();
+	}
+	
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTimeApplication.class, args);
+    }
+}
