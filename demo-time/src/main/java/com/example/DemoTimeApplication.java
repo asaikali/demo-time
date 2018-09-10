8e39c40477e3098b0d070da6fce54fae6a32416b
Adib Saikali <asaikali@users.noreply.github.com>
2018-09-10 11:57:27 -0400

    version 7

--- demo-time/src/main/java/com/example/DemoTimeApplication.java
+++ demo-time/src/main/java/com/example/DemoTimeApplication.java
@@ -14,7 +14,7 @@ public class DemoTimeApplication {
 	@RequestMapping("/")
 	public String home()
 	{
-		return "Version 6: " + new Date();
+		return "Version 7: " + new Date();
 	}
 	
     public static void main(String[] args) {
