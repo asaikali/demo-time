96aea45dd6c3cde34971eb1f9301dac68633bc9f
asaikali <asaikali@gmail.com>
2023-03-08 09:32:13 -0500

    version 2

--- demo-time/src/main/java/com/example/DemoTimeApplication.java
+++ demo-time/src/main/java/com/example/DemoTimeApplication.java
@@ -14,7 +14,7 @@ public class DemoTimeApplication {
 	@RequestMapping("/")
 	public String home()
 	{
-		return "Version 1:  " + new Date();
+		return "Version 2:  " + new Date();
 	}
 	
     public static void main(String[] args) {
